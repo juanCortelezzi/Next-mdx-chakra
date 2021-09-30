47c9519b639baa20a3b37ad6f929a8ca28cf0949
juancortelezzi <juanbicortelezzi@gmail.com>
2021-09-30 20:17:32 -0300

    added types to lib mdx

--- src/lib/mdx.ts
+++ src/lib/mdx.ts
@@ -1,42 +1,57 @@
-import fs from 'fs';
-import path from 'path';
-import { serialize } from 'next-mdx-remote/serialize';
-import matter from 'gray-matter';
-import IFrontMatter from "@typedefs/frontMatter"
+import fs from "fs";
+import path from "path";
+import { serialize } from "next-mdx-remote/serialize";
+import matter from "gray-matter";
+import IFrontMatter from "@typedefs/frontMatter";
+import { MDXRemoteSerializeResult } from "next-mdx-remote";
 
-const postPath = path.join(process.cwd(), 'src', 'posts');
+const postPath = path.join(process.cwd(), "src", "posts");
 
 export async function getFiles() {
-    return fs.readdirSync(postPath);
+  return fs.readdirSync(postPath);
 }
 
-export async function getFileBySlug(slug: string) {
-    const source = fs.readFileSync(path.join(postPath, `${slug}.mdx`), 'utf8')
+interface IExtendedFrontMatter extends IFrontMatter {
+  wordCount: number;
+}
 
-    const { data, content } = matter(source);
-    const mdxSource = await serialize(content);
+export interface IGetFileBySlugResponse {
+  mdxSource: MDXRemoteSerializeResult<Record<string, unknown>>;
+  frontMatter: IExtendedFrontMatter;
+}
 
-    return {
-        mdxSource,
-        frontMatter: {
-            wordCount: content.split(/\s+/gu).length,
-            slug: slug || null,
-            ...data
-        }
-    };
+export async function getFileBySlug(
+  slug: string
+): Promise<IGetFileBySlugResponse> {
+  const source = fs.readFileSync(path.join(postPath, `${slug}.mdx`), "utf8");
+
+  const { data, content } = matter(source);
+  const mdxSource = await serialize(content);
+
+  return {
+    mdxSource,
+    frontMatter: {
+      ...(data as IFrontMatter),
+      wordCount: content.split(/\s+/gu).length,
+      slug: slug,
+    },
+  };
 }
 
 export async function getAllFilesFrontMatter() {
-    const files = fs.readdirSync(postPath);
+  const files = fs.readdirSync(postPath);
 
-    return files.reduce((reducer: IFrontMatter[], postSlug): IFrontMatter[] => {
-        const source = fs.readFileSync(
-            path.join(postPath, postSlug),
-            'utf8'
-        );
+  return files.reduce((reducer: IFrontMatter[], postSlug): IFrontMatter[] => {
+    const source = fs.readFileSync(path.join(postPath, postSlug), "utf8");
 
-        const frontMatterInfo = matter(source);
+    const frontMatterInfo = matter(source);
 
-        return [{ ...frontMatterInfo.data as IFrontMatter, slug: postSlug.replace('.mdx', '') }, ...reducer];
-    }, []);
+    return [
+      {
+        ...(frontMatterInfo.data as IFrontMatter),
+        slug: postSlug.replace(".mdx", ""),
+      },
+      ...reducer,
+    ];
+  }, []);
 }
